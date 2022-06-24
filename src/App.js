4689da8cd103b0f6c93b874cd9086b5de80d2512
Hazari <79424896+Hazaricoolbudy@users.noreply.github.com>
2022-06-24 13:06:39 +0530

    hooks revision app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+// import Accordians from './components/Accordians';
+import Searchterm from './components/Searchterm';
+const items = [
+  {
+    title: 'What is React ',
+    content: 'react is front end javascript library'
+  },
+
+  {
+    title: "why used react",
+    content: "it is used to make components of a page which can be used many times"
+  },
+  {
+    title: 'how do you used react',
+    content: 'we can used react by creating components'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container     ">
+      {/* <Accordians items={items} />*/}
+      <Searchterm />
+
     </div>
   );
 }
---
+++ src/components/Searchterm.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Searchterm = () => {
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label htmlFor="entersearch">Enter Search term</label>
+                    <input type="text" className='input' id='entersearch' />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Searchterm
